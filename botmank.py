27caff41019cc7e54ff51a291899a326136f24c3
Manomay Kagalkar <manomay111@gmail.com>
2022-01-30 15:31:29 +0530

    Update botmank.py

--- botmank.py
+++ botmank.py
@@ -37,14 +37,15 @@ async def on_message(message):
         await message.channel.send(url[choice])
 
     if message.content.startswith("mank joke"):
-        reddit = praw.Reddit(client_id=os.getenv("REDDITID"),
-                             client_secret=os.getenv("REDDITPASS"),
-                             user_agent="botmank")
+        reddit = praw.Reddit(
+        client_id = os.getenv("PRAWID"),
+        client_secret=os.getenv("PRAWPASS"),
+        user_agent="botmank")
         url = []
         for submission in reddit.subreddit("jokes").hot(limit=100):
-            url.append(submission.url)
+            url.append(submission.selftext)
         list1 = range(1, 100, 1)
         choice = int(random.choice(list1))
         await message.channel.send(url[choice])
-        
+
 client.run(os.getenv("TOKEN"))
