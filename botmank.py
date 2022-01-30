9858d7195da3e03eb05d9718a2191c117f744862
Manomay Kagalkar <manomay111@gmail.com>
2022-01-30 16:00:11 +0530

    added jokes and help

--- botmank.py
+++ botmank.py
@@ -25,10 +25,14 @@ async def on_message(message):
     if message.content.startswith("mank ping"):
         await message.channel.send("pong! I'm awake")
 
+    if message.content.startswith("mank help"):
+      await message.channel.send("botmank is a discord bot that responds to messages starting with mank. I'll be gradually adding music, role management, quirky replies and other functionality. It has these basic commands right now -\n\t**ping** - responds if online\n\t**hello** - responds with heyy\n\t**meme** - sends a random meme from r/memes hot 100\n\t**joke** - sends a lame joke from r/jokes hot 100")
+
     if message.content.startswith("mank meme"):
-        reddit = praw.Reddit(client_id=os.getenv("REDDITID"),
-                             client_secret=os.getenv("REDDITPASS"),
-                             user_agent="botmank")
+        reddit = praw.Reddit(
+        client_id = os.getenv("REDDITID"),
+        client_secret=os.getenv("REDDITPASS"),
+        user_agent="botmank")
         url = []
         for submission in reddit.subreddit("memes").hot(limit=100):
             url.append(submission.url)
@@ -43,7 +47,7 @@ async def on_message(message):
         user_agent="botmank")
         url = []
         for submission in reddit.subreddit("jokes").hot(limit=100):
-            url.append(submission.selftext)
+            url.append(submission.title+submission.selftext)
         list1 = range(1, 100, 1)
         choice = int(random.choice(list1))
         await message.channel.send(url[choice])
